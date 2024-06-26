75101cf28b97e28863e57fc0b1f0cbb6fad7ac25
Hamid <HamidSaliqa>
2024-06-26 15:58:57 +0430

    initial Commit

---
+++ lib/src/screens/login.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'package:power_tailor/src/common_widgets/custom_textfield.dart';
+import 'package:power_tailor/src/common_widgets/custom_button.dart';
+import 'package:power_tailor/src/constants/colors.dart';
+
+import '../common_widgets/back_button.dart';
+import '../common_widgets/google_button.dart';
+import 'login_or_signup.dart';
+
+class Login extends StatefulWidget {
+  const Login({super.key});
+
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: p1,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                backIcon(context, onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const LoginOrSignUp(),
+                      ));
+                }),
+                const Padding(
+                  padding: EdgeInsets.only(right: 20),
+                  child: Align(
+                    alignment: Alignment.centerRight,
+                    child: Text(
+                      "ورود",
+                      textDirection: TextDirection.rtl,
+                      style: TextStyle(
+                        color: p2,
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+                googleButton(),
+                customTextField(hintText: "ایمیل"),
+                customTextField(
+                  hintText: "رمز عبور",
+                  obscureText: true,
+                ),
+                customTextField(
+                  hintText: "لایسنس",
+                ),
+                MaterialButton(
+                    onPressed: () {},
+                    child: const Text(
+                      "رمز خود را فراموش کرده اید؟",
+                      style: TextStyle(
+                          color: p2, fontSize: 15, fontWeight: FontWeight.bold),
+                    )),
+                MaterialButton(
+                    onPressed: () {},
+                    child: const Text(
+                      "لایسنس خود را فراموش کرده اید؟",
+                      style: TextStyle(
+                          color: p2, fontSize: 15, fontWeight: FontWeight.bold),
+                    )),
+                const SizedBox(
+                  height: 20,
+                ),
+                customButton(textButton: "ورود به حساب", onPressed: () {}),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/src/screens/login_or_signup.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:power_tailor/src/common_widgets/custom_button.dart';
+import 'package:power_tailor/src/constants/colors.dart';
+import 'package:power_tailor/src/screens/sign_up.dart';
+import 'package:power_tailor/src/screens/start.dart';
+
+
+
+import '../common_widgets/back_button.dart';
+import 'login.dart';
+
+class LoginOrSignUp extends StatelessWidget {
+  const LoginOrSignUp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      backgroundColor: p1,
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              backIcon(context,onPressed: (){
+                Navigator.push(context, MaterialPageRoute(builder: (context) => const Start(),));
+              }),
+              SvgPicture.asset(
+                "assets/images/tailor1.svg",
+                width: 200,
+                height: 200,
+                color: p2,
+              ),
+            
+              customButton(
+                textButton: "ورورد به حساب",
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const Login(),
+                      ));
+                },
+              ),
+           
+              customButton(
+                  textButton: "ساخت حساب جدید", onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const SignUp(),
+                    ));
+              }, width: 250),
+            
+              Image.asset("assets/images/TechLogo.png", height: 100),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
